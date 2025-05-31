a7d384b9f2d92e8e5745fb9b490cb37cb9621afe
RyanBuck182 <RyanBuck182@gmail.com>
2025-05-31 01:00:08 -0400

    Add compose starter files

---
+++ app/src/main/java/com/gmail/ryanbuck182/github_streak/ui/theme/Color.kt
@@ -0,0 +1,7 @@
+package com.gmail.ryanbuck182.github_streak.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Black = Color(0xFF000000)
+val Grey = Color(0xFF7F7F7F)
+val White = Color(0xFFFFFFFF)
